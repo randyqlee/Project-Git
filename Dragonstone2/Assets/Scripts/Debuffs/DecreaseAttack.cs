c05dde6ea1d371a325ab1dc6378ba489d68bbce3
playstrongph <playstrong.ph@gmail.com>
2018-12-08 22:08:26 +0800

    Debuffs
    Debuffs - Decrease Attack and Decrease Defense

---
+++ Dragonstone2/Assets/Scripts/Debuffs/DecreaseAttack.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class DecreaseAttack : Buff {
+
+
+
+//Decrease attack damage by 60	
+int temp;
+
+void Awake (){
+
+	//get debuff asset
+	this.buff = Resources.Load<BuffAsset>("SO Assets/Debuff/Decrease Attack");
+
+	//attach debuff Icon to Hero UI
+	this.buffIcon = buff.icon;
+
+	//apply effect
+	gameObject.GetComponent<HeroManager>().attack -= buff.value;
+
+	
+	}//Awake
+
+	void Update(){}
+
+	void OnEnable(){}
+            
+	protected override void OnDestroy()
+	{
+
+		
+		//remove effect	
+		gameObject.GetComponent<HeroManager>().attack += buff.value;
+
+		//call parent OnDestroy
+		base.OnDestroy();
+
+	}
+
+
+	
+
+}//Class
---
+++ Dragonstone2/Assets/Scripts/Debuffs/DecreaseDefense.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DecreaseDefense : Buff {
+
+	
+
+	// Use this for initialization
+	void Awake () {
+
+		//get buff asset
+		this.buff = Resources.Load<BuffAsset>("SO Assets/Debuff/Decrease Defense");
+
+		//attach buff icon to Hero UI
+		this.buffIcon = buff.icon;
+		
+		//apply effect
+		gameObject.GetComponent<HeroManager>().defense -= buff.value;		
+	}
+
+		
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+	protected override void OnDestroy(){
+
+		//remove effect
+		gameObject.GetComponent<HeroManager>().defense += buff.value;	
+
+		//call parent OnDestroy
+		base.OnDestroy();
+
+	}//OnDestroy
+}
