7284d0aadb958abeb1df98914c19d67167b936fa
playstrongph <playstrong.ph@gmail.com>
2018-12-18 06:39:25 +0800

    Skill Text Canvas
    UI update Skill Text Canvass.  Reference resolution 400 x 800

---
+++ Dragonstone2/Assets/Scripts/Ability Scripts/YeonHong/GracefulTouch.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GracefulTouch : Ability {
+
+	public override void UseAbility(HeroManager attacker, HeroManager defender){
+
+		GameManager.Instance.Attack(attacker, defender);
+		base.UseAbility(attacker, defender);		
+	}
+
+}
