d4ac5e7753a7edee6fdd78976ce9aab971066be9
playstrongph <playstrong.ph@gmail.com>
2019-01-04 13:00:00 +0800

    Ganymede partial

---
+++ Dragonstone2/Assets/Scripts/Ability Scripts/Ganymede/SealMagic.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SealMagic : Ability {
+
+	public override void UseAbility (HeroManager attacker, HeroManager defender)
+	{
+		
+		//Attack an enemy
+		GameManager.Instance.Attack(attacker, defender);
+
+		//Reset Skills to Max Cooldown
+		defender.heroPanel.SetActive(true);
+		Ability[] abilities = defender.GetComponentsInChildren<Ability>();
+		foreach (Ability ability in abilities){
+			//Debug.Log("Abiltiies: " +ability.name);
+			ability.ResetCooldown();
+		}
+		defender.heroPanel.SetActive(false);
+
+		//Gain an Extra turn
+		GameManager.Instance.ExtraTurn();
+
+		base.UseAbility();
+
+
+
+	}//UseAbility
+
+
+}
