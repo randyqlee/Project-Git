db2db01a1f18e51c3ec581a76168afe83f6a7297
randyqlee <44831457+randyqlee@users.noreply.github.com>
2020-06-16 11:42:11 +0800

    06162020

--- DS_u2019_4/Assets/Scripts/Logic/Classes/Player.cs
+++ DS_u2019_4/Assets/Scripts/Logic/Classes/Player.cs
@@ -4,5 +4,10 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    // int ID that we get from ID factory
     public int ID;
+    // a Character Asset that contains data about this Hero. ONLY IF NEEDED
+    public CharacterAsset charAsset;
+    // a script with references to all the visual game objects for this player
+    public PlayerArea PArea;
 }
---
+++ DS_u2019_4/Assets/Scripts/Logic/Systems/BattleManager.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleManager : MonoBehaviour
+{
+
+    // for Singleton Pattern
+    public static BattleManager Instance{ get; private set; }
+    public TurnManager tm; 
+    public List<HeroLogic> CreaturesCreatedThisGame;
+    public Player[] Players;
+
+    void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Start()
+    {
+        InitLists();
+        StartCoroutine (BattleLoop());      
+    }
+
+    void InitLists()
+    {
+        CreaturesCreatedThisGame.Clear();
+        
+        Player[] Players = new Player[2]; 
+        Players = GameObject.FindObjectsOfType<Player>();       
+        
+    }
+
+	IEnumerator BattleLoop()
+	{
+        yield return StartCoroutine(InitBattle());
+
+        yield return StartCoroutine(StartBattle());
+
+        StopCoroutine(InitBattle());
+
+    }
+
+    IEnumerator InitBattle()
+    {
+
+        //initialize Heroes (Logic)
+        //initialize Visuals
+        //set Global values
+        //set who starts first
+
+        //clear arrays
+
+
+
+        //initialize Players
+
+        
+
+        yield return null;
+        StopCoroutine(InitBattle());
+
+    }
+
+    IEnumerator StartBattle()
+    {
+
+        //start ATB and TurnManager, other systems
+
+
+        yield return null;
+        StopCoroutine(StartBattle());
+
+    }
+
+
+
+}
